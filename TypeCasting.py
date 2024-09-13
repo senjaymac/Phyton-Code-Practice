f9d59b041710ea463caef6f1e7331c49e4207ae9
Senon <senon.macatangay@achievewithoutborders.com>
2024-09-14 07:18:35 +0800

    TypeCasting finale

--- TypeCasting.py
+++ TypeCasting.py
@@ -9,3 +9,14 @@ GPA = 2.21
 
 # knowing the Type of type("")
 print(type(Is_graduate))
+
+#convert float into int
+GPA = int(GPA)
+
+print(GPA)
+
+#convert int to float
+Int = float(Int)
+
+print(Int)
+
