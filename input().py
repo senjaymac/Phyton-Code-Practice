31c92b73dddb42682d382b52ff405beae7776c57
Senon <senon.macatangay@achievewithoutborders.com>
2024-09-14 08:07:38 +0800

    User Input Testing

---
+++ input().py
@@ -0,0 +1,29 @@
+# function that prompts user input to enter data and return the entered data into a string
+"""
+Name = input("What is your name? ")
+
+# converted age string input into integere in order to perform the addition operation
+Age = int(input("What is your Age? "))
+
+Age = Age + 1
+
+print(f"{Name} is {Age} years old!")
+"""
+
+# Area rectangle Calculator {this is an exercise for the typecasting}
+
+Length = int(input("Input Length "))
+Width = int(input("Input Width "))
+
+Area = Length * Width
+
+print(f"The Area of the rectangle is {Area} ")
+
+#Shopping cart prog
+Item = input("What is the Item? ") 
+Price = int(input("What is the price? "))
+Quantity = int(input("How many? "))
+
+Total = Price*Quantity
+
+print(f"You bought {Item} with the price of {Price} and quantity of {Quantity} your TotalPrice is {Total} ")
